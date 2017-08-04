e31bc2fd602d56eb14d74083ee5eacc5fffd1b09
Stephen James <sajames1958@gmail.com>
2017-08-04 12:27:02 -0500

    added files

---
+++ app/src/main/java/com/example/helloworld/HelloWorldClientInterface.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Stephen on 7/28/2017.
+ */
+
+public interface HelloWorldClientInterface {
+
+    void HelloWorld(String sessionId);
+}
---
+++ app/src/main/java/com/example/helloworld/VoiceInterface.java
@@ -0,0 +1,46 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.util.Log;
+
+import com.netzyn.api.SaApi;
+import com.netzyn.api.SaApiInterface;
+
+public class VoiceInterface implements SaApiInterface {
+    private final String TAG = "VoiceInterface";
+    private final String AppName = "CHANGE_TO_APP_NAME";
+
+    HelloWorldClientInterface client;
+    private SaApi api = null;
+
+    VoiceInterface(HelloWorldClientInterface client, Intent intent)
+    {
+        this.client = client;
+        api = new SaApi(AppName, intent, this);
+    }
+    public void SaEventVoice(String user, String device, String app, final String sessionId, String intent,
+                      final String parm1, final String parm2, final String parm3, final String parm4, final String parm5)
+    {
+        if (intent.compareToIgnoreCase("hello") == 0) {
+            client.HelloWorld(sessionId);
+        }
+    }
+    public void SendResponse(String sessionId, String msg) {
+        api.SaVoiceResponse(sessionId, "ask", msg);
+    }
+
+    public void SaEventLog(String var1)
+    {
+        Log.i(TAG, "Event Log: " + var1);
+    }
+
+    public void SaEventConnected()
+    {
+        Log.i(TAG, "Event Connected");
+    }
+
+    public void SaEventError(String var1)
+    {
+        Log.i(TAG, "Event Error: " + var1);
+    }
+}
