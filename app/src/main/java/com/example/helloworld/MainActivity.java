af78836984c7f2ac9ff727f1f01f3bfe4b6130de
Stephen James <sajames1958@gmail.com>
2017-08-04 12:26:46 -0500

    added files

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HelloWorldClientInterface {
+
+    private VoiceInterface api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        api = new VoiceInterface(this, this.getIntent());
+    }
+
+    void ShowToast(String msg) {
+        Toast toast = Toast.makeText(
+                this.getApplicationContext(),
+                msg,
+                Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void HelloWorld(String sessionId) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ShowToast("Hello received");
+            }
+        });
+
+        api.SendResponse(sessionId, "REPLACE WITH YOUR MESSAGE");
     }
 }
