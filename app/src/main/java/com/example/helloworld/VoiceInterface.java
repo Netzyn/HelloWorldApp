aa8404acc3e2dda56ecea084e2852e530a59b815
Stephen James <sajames1958@gmail.com>
2017-08-23 11:29:13 -0500

    new lib and static API object

--- app/src/main/java/com/example/helloworld/VoiceInterface.java
+++ app/src/main/java/com/example/helloworld/VoiceInterface.java
@@ -6,62 +6,62 @@ import android.util.Log;
 import com.netzyn.api.SaApi;
 import com.netzyn.api.SaApiInterface;
 
-public class VoiceInterface implements SaApiInterface {
-    private final String TAG = "VoiceInterface";
-    private final String AppName = "CHANGE_TO_APP_NAME";
+    public class VoiceInterface implements SaApiInterface {
+        private final String TAG = "VoiceInterface";
+        private final String AppName = "CHANGE_TO_APP_NAME";
 
-    HelloWorldClientInterface client;
-    private SaApi api = null;
+        HelloWorldClientInterface client;
+        private SaApi api = null;
 
-    static private VoiceInterface Instance = null;
-    static VoiceInterface startVoiceInterface(HelloWorldClientInterface client, Intent intent) {
-        if (Instance == null) {
-            Instance = new VoiceInterface(client, intent);
+        static private VoiceInterface Instance = null;
+        static VoiceInterface startVoiceInterface(HelloWorldClientInterface client, Intent intent) {
+            if (Instance == null) {
+                Instance = new VoiceInterface(client, intent);
+            }
+            else
+            {
+                Instance.client = client;
+            }
+            return Instance;
         }
-        else
+
+        public void onDestroy() {
+            client = null;
+        }
+
+        VoiceInterface(HelloWorldClientInterface client, Intent intent)
         {
-            Instance.client = client;
+            this.client = client;
+            api = new SaApi(AppName, intent, this);
         }
-        return Instance;
-    }
+        public void SaEventVoice(String user, String device, String app, final String sessionId, String intent,
+                          final String parm1, final String parm2, final String parm3, final String parm4, final String parm5)
+        {
+            if (client == null) {
+                api.SaVoiceResponse(sessionId, "tell", "Application is not ready.");
+                return;
+            }
 
-    public void onDestroy() {
-        client = null;
-    }
-
-    VoiceInterface(HelloWorldClientInterface client, Intent intent)
-    {
-        this.client = client;
-        api = new SaApi(AppName, intent, this);
-    }
-    public void SaEventVoice(String user, String device, String app, final String sessionId, String intent,
-                      final String parm1, final String parm2, final String parm3, final String parm4, final String parm5)
-    {
-        if (client == null) {
-            api.SaVoiceResponse(sessionId, "tell", "Application is not ready.");
-            return;
+            if (intent.compareToIgnoreCase("hello") == 0) {
+                client.HelloWorld(sessionId);
+            }
+        }
+        public void SendResponse(String sessionId, String msg) {
+            api.SaVoiceResponse(sessionId, "ask", msg);
         }
 
-        if (intent.compareToIgnoreCase("hello") == 0) {
-            client.HelloWorld(sessionId);
+        public void SaEventLog(String var1)
+        {
+            Log.i(TAG, "Event Log: " + var1);
+        }
+
+        public void SaEventConnected()
+        {
+            Log.i(TAG, "Event Connected");
+        }
+
+        public void SaEventError(String var1)
+        {
+            Log.i(TAG, "Event Error: " + var1);
         }
     }
-    public void SendResponse(String sessionId, String msg) {
-        api.SaVoiceResponse(sessionId, "ask", msg);
-    }
-
-    public void SaEventLog(String var1)
-    {
-        Log.i(TAG, "Event Log: " + var1);
-    }
-
-    public void SaEventConnected()
-    {
-        Log.i(TAG, "Event Connected");
-    }
-
-    public void SaEventError(String var1)
-    {
-        Log.i(TAG, "Event Error: " + var1);
-    }
-}
